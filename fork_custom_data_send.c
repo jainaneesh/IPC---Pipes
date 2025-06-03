0b661e1d7dea5f1df00be6e7ff9f720060829a4c
jainaneesh <jainaneesh15@gmail.com>
2025-06-03 00:28:04 -0400

    Exrtapolated learning to sending custom data to the child

--- fork_custom_data_send.c
+++ fork_custom_data_send.c
@@ -6,11 +6,11 @@
 
 #define BUFFER_SIZE 100
 
-int main() {
+int main(int argc, char *argv[]) {
     int pipefd[2];
     pid_t pid;
     char buffer[BUFFER_SIZE];
-    const char *message = "Hello from parent!";
+    char *message;
 
     if (pipe(pipefd) == -1) {
         perror("pipe");
@@ -23,6 +23,16 @@ int main() {
         perror("fork");
         exit(EXIT_FAILURE);
     }
+    
+    if ( argc == 2 ){
+    	message = strdup(argv[1]);
+	if ( !message ){
+		perror("strdup");
+		exit(EXIT_FAILURE);
+	}
+    } else {
+	message = "Hello";
+    }
 
     if (pid == 0) {
         // Child Process
@@ -31,17 +41,26 @@ int main() {
         ssize_t bytes = read(pipefd[0], buffer, BUFFER_SIZE);
         if (bytes > 0) {
             buffer[bytes] = '\0';
-            printf("Child received: %s\n", buffer);
+
+	    // Checking what was received
+	    if ( strcmp(buffer, "0xFF") == 0 ) {
+		    printf("Turning on the lights\r\n");
+	    } else if ( strcmp(buffer, "0xFE") == 0 ) {
+		    printf("Turning on the AC\r\n");
+	    } else {
+		    printf("Invalid option\r\n");
+	    }
+            //printf("Child received: %s\n", buffer);
         }
 
 	// Keep the child alive for inspection
-	sleep(20);
+	//sleep(20);
         close(pipefd[0]);
     } else {
         // Parent Process
         close(pipefd[0]);  // Close read end
 	printf("Sending data to the child\r\n");
-	sleep(20);
+	//sleep(20);
         write(pipefd[1], message, strlen(message));
         printf("Parent sent: %s\n", message);
         close(pipefd[1]);
